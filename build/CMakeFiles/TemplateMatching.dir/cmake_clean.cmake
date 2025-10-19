fe23cf0aaa0109c3f41d354e51531447311d3c75
Elad <eladima66@gmail.com>
2025-10-19 05:02:44 +0300

    Initial Commit

---
+++ build/CMakeFiles/TemplateMatching.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TemplateMatching.dir/TemplateMatching.cpp.o"
+  "CMakeFiles/TemplateMatching.dir/TemplateMatching.cpp.o.d"
+  "libTemplateMatching.a"
+  "libTemplateMatching.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TemplateMatching.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
